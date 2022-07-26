785cf5073c0930653f3fc624e28448638c5fcb09
FredLitt <80988264+FredLitt@users.noreply.github.com>
2022-07-26 11:23:29 -0700

    added inputs for country names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [ countries, setCountry ] = useState({ firstCountry: null, secondCountry: null })
+
+  const [ firstCountry, setFirstCountry ] = useState("")
+  const [ secondCountry, setSecondCountry ] = useState("")
+console.log(firstCountry, secondCountry)
   return (
     <div className="App">
-      
+      <div>
+        <h1>Country Compare</h1>
+        <input value={firstCountry} onChange={(e) => setFirstCountry(e.target.value)} type="text" />
+        <input value={secondCountry} onChange={(e) => setSecondCountry(e.target.value)} type="text" />
+        <button type="button">Compare!</button>
+
+        <table>
+
+        </table>
+      </div>
     </div>
   );
 }
