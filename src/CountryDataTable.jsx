69137820186a1e4d3f1662ad8c971f85e217c75a
FredLitt <80988264+FredLitt@users.noreply.github.com>
2022-07-29 13:18:36 -0700

    added renderData helper function to table

--- src/CountryDataTable.jsx
+++ src/CountryDataTable.jsx
@@ -1,4 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function renderData(data){
+  if (data.key === "Flag"){
+    return (
+      <>
+        <td><img src={data.firstCountry} alt=""/></td>
+        <td><img src={data.secondCountry} alt=""/></td>
+      </>
+    )
+  }
+  if (data.key === "Population"){
+    return (
+      <>
+        <td>{data.firstCountry.toLocaleString()}</td>
+        <td>{data.secondCountry.toLocaleString()}</td>
+      </>
+    )
+  } 
+  if (data.key === "Area"){
+    return (
+      <>
+        <td>{data.firstCountry.toLocaleString()} km<sup>2</sup></td>
+        <td>{data.secondCountry.toLocaleString()} km<sup>2</sup></td>
+      </>
+    )
+  } 
+  return (
+    <>
+      <td>{data.firstCountry}</td>
+      <td>{data.secondCountry}</td>
+    </>
+  )
+}
 
 export default function CountryDataTable({ countryData }) {
 
@@ -8,15 +41,7 @@ export default function CountryDataTable({ countryData }) {
         {countryData.map((data, index) =>
         <tr key={index}>
             <td>{data.key}</td>
-            {data.key === "Flag" ? 
-            <>
-              <td><img src={data.firstCountry} alt=""/></td>
-              <td><img src={data.secondCountry} alt=""/></td>
-            </> :
-            <>
-              <td>{data.firstCountry}</td>
-              <td>{data.secondCountry}</td>
-            </>}
+            {renderData(data)}
           </tr>)}
         </tbody>
       </table>
