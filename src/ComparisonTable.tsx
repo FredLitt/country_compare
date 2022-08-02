30f8492c9aee5dd4f99c5e1331eed449b7847e82
FredLitt <80988264+FredLitt@users.noreply.github.com>
2022-08-02 11:41:29 -0700

    added function to compare country values

--- src/ComparisonTable.tsx
+++ src/ComparisonTable.tsx
@@ -1,31 +1,53 @@
 import React from "react";
-import { Datapoint } from "./types";
+import { CountryData } from "./types";
 
-export default function ComparisonTable({ countryData }: any) {
-  const findPopulationDensity = (countryData: Datapoint[]) => {
-    const populationData = countryData.find(
-      (data) => data.key === "Population"
-    );
-    const areaData = countryData.find((data) => data.key === "Area");
-    if (!populationData || !areaData) return;
-    const countryAPopulationDensity = Math.round(
-      parseInt(populationData.firstCountry) / parseInt(areaData.firstCountry)
-    );
-    console.log(countryAPopulationDensity);
-    return countryAPopulationDensity;
+export default function ComparisonTable({ firstCountry, secondCountry }: any) {
+  interface ComparisonOption {
+    country: CountryData;
+    value: number;
+  }
+
+  const compareCountryValues = (
+    first: ComparisonOption,
+    second: ComparisonOption,
+    propertyName: string
+  ) => {
+    const higherValue = Math.max(first.value, second.value);
+    const lowerValue = Math.min(first.value, second.value);
+    const multiple = Number(higherValue / lowerValue).toFixed(2);
+    const countryWithHigherValue =
+      first.value === higherValue ? first.country : second.country;
+    const countryWithLowerValue =
+      first.value === lowerValue ? first.country : second.country;
+    return `${countryWithHigherValue.country} has ${multiple} times the ${propertyName} of ${countryWithLowerValue.country}`;
   };
-  findPopulationDensity(countryData);
-  //   const comparisonData: any[] = [
-  //     {
-  //       name: "Population Density",
-  //       firstCountry: findPopulationDensity(countryData[0]),
-  //       secondCountry: findPopulationDensity(countryData[1]),
-  //     },
-  //   ];
 
   return (
     <table>
-      <tbody></tbody>
+      <tbody>
+        <tr>
+          {compareCountryValues(
+            { country: firstCountry, value: firstCountry.population },
+            { country: secondCountry, value: secondCountry.population },
+            "population"
+          )}
+        </tr>
+        <tr>
+          {compareCountryValues(
+            { country: firstCountry, value: firstCountry.area },
+            { country: secondCountry, value: secondCountry.area },
+            "area"
+          )}
+        </tr>
+        <tr>
+          {compareCountryValues(
+            { country: firstCountry, value: firstCountry.population_density },
+            { country: secondCountry, value: secondCountry.population_density },
+            "population density"
+          )}
+        </tr>
+        <></>
+      </tbody>
     </table>
   );
 }
